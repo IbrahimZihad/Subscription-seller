5bff7efc616324ff44525860f1440b8fd6b359bb
IbrahimZihad <ibrahimzihad5@gmail.com>
2026-05-21 01:12:24 +0600

    build changes

--- subscriptionsbd/.next/static/chunks/webpack.js
+++ subscriptionsbd/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b5026ddf2f6ab24"; }
+/******/ 		__webpack_require__.h = function() { return "9ea1cfeeaae37fcd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
